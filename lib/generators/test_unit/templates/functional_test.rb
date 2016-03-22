c3be093654ecd788e05ffc5720a0df224a6871da
Akira Matsuda <ronnie@dio.jp>
2016-03-22 16:30:53 +0900

    Needless to_param

--- lib/generators/test_unit/templates/functional_test.rb
+++ lib/generators/test_unit/templates/functional_test.rb
@@ -26,23 +26,23 @@ class <%= controller_class_name %>ControllerTest < ActionController::TestCase
   end
 
   test "should show <%= singular_table_name %>" do
-    get :show, :<%= nested_parent_id %> => @<%= nested_parent_name %>, :id => @<%= singular_table_name %>.to_param
+    get :show, :<%= nested_parent_id %> => @<%= nested_parent_name %>, :id => @<%= singular_table_name %>
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :<%= nested_parent_id %> => @<%= nested_parent_name %>, :id => @<%= singular_table_name %>.to_param
+    get :edit, :<%= nested_parent_id %> => @<%= nested_parent_name %>, :id => @<%= singular_table_name %>
     assert_response :success
   end
 
   test "should update <%= singular_table_name %>" do
-    put :update, :<%= nested_parent_id %> => @<%= nested_parent_name %>, :id => @<%= singular_table_name %>.to_param, :<%= singular_table_name %> => @<%= singular_table_name %>.attributes
+    put :update, :<%= nested_parent_id %> => @<%= nested_parent_name %>, :id => @<%= singular_table_name %>, :<%= singular_table_name %> => @<%= singular_table_name %>.attributes
     assert_redirected_to <%= nested_parent_name %>_<%= singular_table_name %>_path(@<%= nested_parent_name %>, assigns(:<%= singular_table_name %>))
   end
 
   test "should destroy <%= singular_table_name %>" do
     assert_difference('<%= class_name %>.count', -1) do
-      delete :destroy, :<%= nested_parent_id %> => @<%= nested_parent_name %>, :id => @<%= singular_table_name %>.to_param
+      delete :destroy, :<%= nested_parent_id %> => @<%= nested_parent_name %>, :id => @<%= singular_table_name %>
     end
 
     assert_redirected_to <%= nested_parent_name %>_<%= index_helper %>_path(@<%= nested_parent_name %>)

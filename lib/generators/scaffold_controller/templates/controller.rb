a1f19f8281b52d027024a09720eeaed7a1103ebf
Akira Matsuda <ronnie@dio.jp>
2016-03-22 09:54:26 +0900

    DHH style filter

--- lib/generators/scaffold_controller/templates/controller.rb
+++ lib/generators/scaffold_controller/templates/controller.rb
@@ -1,4 +1,6 @@
 class <%= controller_class_name %>Controller < ApplicationController
+  before_action :set_<%= singular_table_name %>, only: [:show, :edit, :update, :destroy]
+
   # GET <%= plural_nested_parent_name %>/1/<%= plural_name %>
   def index
     @<%= nested_parent_name %> = <%= orm_class.find(nested_parent_class_name, "params[:#{nested_parent_id}]") %>
@@ -7,8 +9,6 @@ class <%= controller_class_name %>Controller < ApplicationController
 
   # GET <%= plural_nested_parent_name %>/1/<%= plural_name %>/1
   def show
-    @<%= nested_parent_name %> = <%= orm_class.find(nested_parent_class_name, "params[:#{nested_parent_id}]") %>
-    <%= "@#{singular_name} = @#{nested_parent_name}.#{plural_name}.find(params[:id])" %>
   end
 
   # GET <%= plural_nested_parent_name %>/1/<%= plural_name %>/new
@@ -19,8 +19,6 @@ class <%= controller_class_name %>Controller < ApplicationController
 
   # GET <%= plural_nested_parent_name %>/1/<%= plural_name %>/1/edit
   def edit
-    @<%= nested_parent_name %> = <%= orm_class.find(nested_parent_class_name, "params[:#{nested_parent_id}]") %>
-    <%= "@#{singular_name} = @#{nested_parent_name}.#{plural_name}.find(params[:id])" %>
   end
 
   # POST <%= plural_nested_parent_name %>/1/<%= plural_name %>
@@ -37,9 +35,6 @@ class <%= controller_class_name %>Controller < ApplicationController
 
   # PUT <%= plural_nested_parent_name %>/1/<%= plural_name %>/1
   def update
-    @<%= nested_parent_name %> = <%= orm_class.find(nested_parent_class_name, "params[:#{nested_parent_id}]") %>
-    <%= "@#{singular_name} = @#{nested_parent_name}.#{plural_name}.find(params[:id])" %>
-
     if @<%= singular_name %>.update_attributes(params[:<%= singular_name %>])
       redirect_to([@<%= singular_name %>.<%= nested_parent_name %>, @<%= singular_name %>], :notice => '<%= human_name %> was successfully updated.')
     else
@@ -49,10 +44,15 @@ class <%= controller_class_name %>Controller < ApplicationController
 
   # DELETE <%= plural_nested_parent_name %>/1/<%= plural_name %>/1
   def destroy
-    @<%= nested_parent_name %> = <%= orm_class.find(nested_parent_class_name, "params[:#{nested_parent_id}]") %>
-    <%= "@#{singular_name} = @#{nested_parent_name}.#{plural_name}.find(params[:id])" %>
     @<%= singular_name %>.destroy
 
     redirect_to <%= nested_parent_name %>_<%= index_helper %>_url(@<%= nested_parent_name %>)
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_<%= singular_table_name %>
+      @<%= nested_parent_name %> = <%= orm_class.find(nested_parent_class_name, "params[:#{nested_parent_id}]") %>
+      <%= "@#{singular_name} = @#{nested_parent_name}.#{plural_name}.find(params[:id])" %>
+    end
 end

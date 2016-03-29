ebd220fcafb0a0b1e2340165f11db978d242b47f
Akira Matsuda <ronnie@dio.jp>
2016-03-29 21:27:35 +0900

    Add `references` to fixtures as well

--- lib/generators/test_unit/model_generator.rb
+++ lib/generators/test_unit/model_generator.rb
@@ -8,6 +8,15 @@ module NestedScaffold
         include ::NestedScaffold::Base
 
         source_root superclass.source_root
+
+        def initialize(*)
+          super
+
+          unless attributes.map {|a| [a.name, a.type.to_s]}.include? [nested_parent_name, 'references']
+            #DIRTY HACK add 'references' attribute
+            attributes.insert 0, Rails::Generators::GeneratedAttribute.new(nested_parent_name, :references)
+          end
+        end
       end
     end
   end

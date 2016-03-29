6ff6f5aa973759d76fcf9d4c249da970aafb2215
Akira Matsuda <ronnie@dio.jp>
2016-03-29 17:41:36 +0900

    Remove `assigns` from functional_test templates

--- lib/generators/test_unit/templates/functional_test.rb
+++ lib/generators/test_unit/templates/functional_test.rb
@@ -9,7 +9,6 @@ class <%= controller_class_name %>ControllerTest < ActionController::TestCase
   test "should get index" do
     get :index, params: { <%= nested_parent_id %>: @<%= nested_parent_name %> }
     assert_response :success
-    assert_not_nil assigns(:<%= table_name %>)
   end
 
   test "should get new" do
@@ -22,7 +21,7 @@ class <%= controller_class_name %>ControllerTest < ActionController::TestCase
       post :create, params: { <%= nested_parent_id %>: @<%= nested_parent_name %>, <%= singular_table_name %>: @<%= singular_table_name %>.attributes }
     end
 
-    assert_redirected_to <%= nested_parent_name %>_<%= singular_table_name %>_path(@<%= nested_parent_name %>, assigns(:<%= singular_table_name %>))
+    assert_redirected_to <%= nested_parent_name %>_<%= singular_table_name %>_path(@<%= nested_parent_name %>, <%= class_name %>.last)
   end
 
   test "should show <%= singular_table_name %>" do
@@ -37,7 +36,7 @@ class <%= controller_class_name %>ControllerTest < ActionController::TestCase
 
   test "should update <%= singular_table_name %>" do
     put :update, params: { <%= nested_parent_id %>: @<%= nested_parent_name %>, id: @<%= singular_table_name %>, <%= singular_table_name %>: @<%= singular_table_name %>.attributes }
-    assert_redirected_to <%= nested_parent_name %>_<%= singular_table_name %>_path(@<%= nested_parent_name %>, assigns(:<%= singular_table_name %>))
+    assert_redirected_to <%= nested_parent_name %>_<%= singular_table_name %>_path(@<%= nested_parent_name %>, <%= class_name %>.last)
   end
 
   test "should destroy <%= singular_table_name %>" do

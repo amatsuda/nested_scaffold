e1f46e8c5cef7f9034c7acb835b2cef1e749920f
Akira Matsuda <ronnie@dio.jp>
2016-03-31 09:30:33 +0900

    Clean up :put_litter_in_its_place:

--- test/nested_scaffold_test.rb
+++ test/nested_scaffold_test.rb
@@ -14,13 +14,9 @@ class NestedScaffoldTest < Rails::Generators::TestCase
   Detail = "sweet"
 
   test "Assert all files are properly created" do
-
     res = "#{Master}/#{Detail}"
     out = run_generator %w(#{res}/sweet name:string quantity:integer notes:text --orm=active_record --test-framework=test_unit --migration=true --fixture)
     puts "\nOutput from generator:\n" + out
-
-    #ls "app/assets/stylesheets"
-    #ls "db/migrate"
 
     assert_file "app/controllers/#{Detail}s_controller.rb"
     assert_file "test/controllers/#{Detail}s_controller_test.rb"
@@ -34,13 +30,10 @@ class NestedScaffoldTest < Rails::Generators::TestCase
     assert_file "app/views/#{Detail}s/new.html.erb"
     assert_file "app/views/#{Detail}s/edit.html.erb"
 
-
     assert_file "app/models/#{Detail}.rb"
     assert_file "test/fixtures/#{Detail}s.yml"
     assert_file "test/models/#{Detail}_test.rb"
     assert_migration "db/migrate/create_#{Detail}s.rb"
-
-
   end
 
 private
@@ -52,15 +45,6 @@ private
       FileUtils.mkdir("db")
       FileUtils.mkdir("db/migrate")
       FileUtils.touch("db/migrate/20120313224522_create_#{Master}.rb")
-
-    end
-  end
-
-  def ls(path)
-    d = File.expand_path("../tmp/"+path, File.dirname(__FILE__))
-    puts "\nOutput from ls (#{d.inspect}):"
-    Dir.foreach(d) do |f|
-      puts f.inspect
     end
   end
 end
